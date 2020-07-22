a085166109527b5e9e0cc3d159702f2a274fba4d
SongyanYu <songyan.yu@griffithuni.edu.au>
2020-07-22 12:30:47 +1000

    delete example data for EMS publication

--- example/R script/Post-processing visualisation.R
+++
@@ -1,7 +0,0 @@
-#---
-# This script visualise DYRESM-CAEDYM modelling outputs for Lake Okareka.
-# This script comes with the publicaiton of R package dycdTools.
-# Author: SOngyan Yu
-# Date created: 03/07/2020
-#---
-
