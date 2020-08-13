b166a728ea7ce5f7f9b0e36250583620e3c61d26
SongyanYu <songyan.yu@griffithuni.edu.au>
2020-08-14 09:44:42 +1000

    add example to extract function

--- R/Fun_Extract-simulation.R
+++ R/Fun_Extract-simulation.R
@@ -4,28 +4,37 @@
 #' Extract simulations from DYRESM-CAEDYM output. It is recommended to use the following example code to
 #' assign values of each simulated variable to a corresponding matrix.
 #'
-#' @param dycd.output the output netcdf file of DYRESM-CAEDYM model
+#' @param dycd.output a string of characters of the output netcdf file names of DYRESM-CAEDYM model
 #' @param var.extract a vector of variables to be extracted from the output file.
 #'    Options include TEMP, DO, TP, NO3, NH4, TN, CHLOR, FDIAT, SSOL1, SSOL2, and PO4.
 #'    Apart from nominated variables, simulation period and layer height data are also extracted.
 #'
 #' @import ncdf4
 #'
+#' @examples
+#'  # extract simulated temperature values from DYRESM-CAEDYM simulation file
+#'  var.values<-extract.output(dycd.output="data/dysim.nc",var.extract=c("TEMP"))
+#'
+#'  for(i in 1:length(var.values)){
+#'   expres<-paste0(names(var.values)[i],"<-data.frame(var.values[[",i,"]])")
+#'   eval(parse(text=expres))
+#'  }
 #'
 #' @return a list of values of those variables of interest,
 #'    as well as two compulsory variables (i.e. simulation period, water level)
 #'
 
-extract.output<-function(dycd.output="DYsim.nc",
+extract.output<-function(dycd.output="data/dysim.nc",
                          var.extract=c("TEMP","DO","TP","TN","NO3","NH4","CHLA")){
+
 
   simData <- nc_open(dycd.output)
   varNames <- names(simData$var)
 
-  cat("You are going to extract",length(var.extract),"variables.\n")
+  message("You are going to extract ",length(var.extract)," variables.\n")
 
   if(any(is.na(match(var.extract,output_name$var.name)))){
-    cat(var.extract[is.na(match(var.extract,output_name$var.name))],"are not on the optional variable list.\n")
+    message(var.extract[is.na(match(var.extract,output_name$var.name))],"are not on the optional variable list.\n")
   }
 
   actual.var<-as.character(na.omit(output_name$output.name[match(var.extract,output_name$var.name)]))
