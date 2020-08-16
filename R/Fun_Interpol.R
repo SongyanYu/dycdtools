9049ad54667d20e28a3fb20a936f607f6845303f
Sunny Yu <s2974665@staff.ad.griffith.edu.au>
2020-08-17 09:37:07 +1000

    add examples

--- R/Fun_Interpol.R
+++ R/Fun_Interpol.R
@@ -1,6 +1,6 @@
-#'Interpolation of simulation along depths.
+#' Interpolation of simulation for a series of user-defined depths.
 #'
-#'Convert simulated variable at irregular layer heights to a dataframe of the same variable at regular (i.e.sequenced) layer heights.
+#' @description Convert simulated variable at irregular layer heights to a dataframe of the same variable at sequenced layer heights.
 #'
 #' @param layerHeights layer heights, extracted from DYCD outputs
 #' @param var simulated variable values, extracted from DYCD outputs
@@ -8,6 +8,20 @@
 #'
 #' @importFrom stats approx na.omit
 #' @return a matrix of interpolated values of such variable.
+#'
+#' @examples
+#' # extract simulated temperature values from DYRESM-CAEDYM simulation file
+#'  var.values<-extract.output(dycd.output="data/dysim.nc",var.extract=c("TEMP"))
+#'
+#'  for(i in 1:length(var.values)){
+#'   expres<-paste0(names(var.values)[i],"<-data.frame(var.values[[",i,"]])")
+#'   eval(parse(text=expres))
+#'  }
+#'  # interpolate temperature for depths from 0 to 13 m at increment of 0.5 m
+#'  temp.interpolated<-interpol(layerHeights = dyresmLAYER_HTS_Var,
+#'                             var = dyresmTEMPTURE_Var,
+#'                             min.dept = 0,max.dept = 13,by.value = 0.5)
+#'
 #' @export
 
 interpol<-function(layerHeights,
