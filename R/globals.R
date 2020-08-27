514dc999f1654659cc64a81eb44501d3b78a6aa0
SongyanYu <songyan.yu@griffithuni.edu.au>
2020-08-27 21:28:41 +1000

    declare a new global variable

--- R/globals.R
+++ R/globals.R
@@ -5,4 +5,5 @@ utils::globalVariables(c("Date",
                          "temp.interpolated",
                          "obs.temp",
                          "Depth",
-                         "."))
+                         ".",
+                         "Value"))
