75c3f3785f0cf0e83a24b8120682a90961ca8579
SongyanYu <songyan.yu@griffithuni.edu.au>
2020-10-25 22:04:18 +1000

    move run.iteration function as a seperate

--- R/Fun_Calib-assist.R
+++ R/Fun_Calib-assist.R
@@ -26,8 +26,6 @@
 #' @param write.out if TRUE, the auto-calibration results are saved a file with a file name set by the "file_name" argument.
 #'
 #' @import dplyr
-#' @importFrom parallel detectCores makeCluster stopCluster clusterExport
-#' @importFrom R.utils copyDirectory
 #' @importFrom utils read.csv write.csv
 #' @return a dataframe of trialed values of parameters and corresponding values of objective function(s).
 #'
@@ -148,74 +146,15 @@ calib.assist<-function(cal.para="Data/Calibration parameters.csv",
       R.utils::copyDirectory(paste0(dycd.wd, "/Files"), to = paste0(dir.core, "/Files"), recursive = T, overwrite = T)
     }
 
-    #### define function to run (and process?) the model on each core
-    run.iteration <- function(this.sim, dycd.wd) {
-
-      # which core to run it on?
-      core <- sim.cores[this.sim]
-
-      # id folder to use
-      dir.this <- paste0(dycd.wd,'/core', core)
-
-      # copy the parameters files from the base model folder into the iteration's folder
-      files.params <- list.files(path = dycd.wd,pattern = '.*\\.(par|chm|bio|sed)$',
-                                 recursive = FALSE, include.dirs = TRUE, full.names = TRUE)
-      file.copy(files.params, dir.this, overwrite = TRUE)
-
-
-      ### !!!!!!! call code to modify the parameters files as needed here
-
-      #---
-      # change the parameter values in the input files
-      #---
-      for(m in 1:ncol(para.df)){
-        change_input_file(input_file = paste0(dir.this,"/",para.raw$Input_file[m]),
-                          row_no = para.raw$Line_NO[m],
-                          new_value = para.df[this.sim,m])
-      }
-
-      #---
-      # model simulation / run the .bat file
-      #---
-      user.wd <- getwd()
-      on.exit(setwd(user.wd))
-      setwd(dir.this)
-      bat.file <- list.files(pattern = ".bat")
-      shell(bat.file,intern = TRUE,wait=TRUE)
-      setwd(user.wd)
-
-      ## verify that the sim has completed successfully
-      log <- file(paste0(dir.this,'/dy.log'))
-      open(log)
-      out <- scan(log,10,what="char(0)",sep="\n",quiet=TRUE)
-
-      while(TRUE){
-        tmp <- scan(log,1,what="char(0)",sep="\n",quiet=TRUE)
-        if(length(tmp)==0) {close(log) ; break }
-        out <- c(out[-1],tmp)
-      }
-
-      if (!TRUE %in% grepl("END: LAKE SIMULATION",out)) {
-        warning(paste0("Simulation No.",this.sim," did not complete successfully.\n"))
-      }
-
-      # copy the finished nc file for later processing (if needed)
-      file.copy(from = paste0(dir.this,"/DYsim.nc"),
-                to = paste0(dir.output,"/DYsim_",this.sim,".nc"))
-
-      ### ! can also add additional model processing here and write to the output folder,
-      ###   to take advantage of parallel speed if wanted
-
-    } # end per core function
-
     ##### initiate and execute the cluster run
     # clean up cluster if left over from last time
-    try({parallel::stopCluster(cl)})
+    #try({parallel::stopCluster(cl)})
 
     cl <- parallel::makeCluster(n.cores)
 
     # export any necessary objects and/or functions to the cluster before running
-    parallel::clusterExport(cl, varlist = c("run.iteration","dycd.wd","dir.output","sim.cores","para.df","change_input_file","para.raw","delete.space"),
+    parallel::clusterEvalQ(cl, library("dycdtools"))
+    parallel::clusterExport(cl, varlist = c("dycd.wd","dir.output","sim.cores","para.df","para.raw"),
                             envir = .GlobalEnv)
 
     parallel::clusterApply(cl, 1:length(iteration), run.iteration, dycd.wd)
@@ -433,7 +372,7 @@ calib.assist<-function(cal.para="Data/Calibration parameters.csv",
                                                var = sim.var,
                                                min.depth = 0,max.depth = max.depth,by.value = 0.5))
 
-        if(class(try.return)!="try-error"){
+        if(class(try.return)[1]!="try-error"){
           if(exists("NSE.list")){
             NSE.list[[var]][i]<-objective.fun(sim = interpolated,
                                               obs = data.frame(obs.list[[index]]),
