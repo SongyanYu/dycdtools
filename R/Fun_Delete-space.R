8c4cd4ae656ba1e06dec54685d3a86620017a2d2
Sunny Yu <s2974665@staff.ad.griffith.edu.au>
2022-04-28 15:31:58 +1000

    reformat delete space function

--- R/Fun_Delete-space.R
+++ R/Fun_Delete-space.R
@@ -2,9 +2,8 @@
 #' @param extract_val a vector.
 #'
 
-delete_space<-function(extract_val){
-  while(extract_val[1]=="")
-  {
+delete_space <- function(extract_val){
+  while(extract_val[1] == ""){
     extract_val<-extract_val[-1]
   }
   return(extract_val)
