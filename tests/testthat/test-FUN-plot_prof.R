3ea49ec7ad4b3f0684ccd8e3355d7ce102d1b44e
Sunny Yu <s2974665@staff.ad.griffith.edu.au>
2022-05-25 15:03:31 +1000

    add unit test

--- tests/testthat/test-FUN-plot_prof.R
+++ tests/testthat/test-FUN-plot_prof.R
@@ -1,11 +1,11 @@
 
 test_that('plot_prof outputs a plot object',{
 
-  obs = data.frame(Date = '2020-01-01',
+  obs <- data.frame(Date = '2020-01-01',
                    Depth = 0:5,
                    TEMP = 29:24)
 
-  sim = matrix(c(28,28,28,27,25,24),
+  sim <- matrix(c(28,28,28,27,25,24),
                nrow = 6,
                ncol = 1)
 
@@ -19,5 +19,29 @@ test_that('plot_prof outputs a plot object',{
             min.depth = 0,
             max.depth = 5,
             by.value = 1)))
+
+})
+
+
+test_that('plot_prof accept right format of date',{
+
+  obs <- data.frame(Date = '2020-01-01',
+                   Depth = 0:5,
+                   TEMP = 29:24)
+
+  sim <- matrix(c(28,28,28,27,25,24),
+               nrow = 6,
+               ncol = 1)
+
+  expect_error(is.object(plot_prof(sim = sim,
+                                  obs = obs,
+                                  sim.start = '01-01-2020',
+                                  sim.end = '2020-01-01',
+                                  plot.start = '2020-01-01',
+                                  plot.end = '2020-01-01',
+                                  xlabel = 'degrees',
+                                  min.depth = 0,
+                                  max.depth = 5,
+                                  by.value = 1)))
 
 })
---
+++ tests/testthat/test-FUN-plot_ts.R
@@ -0,0 +1,46 @@
+test_that('plot_ts outputs a plot object',{
+
+  obs <- data.frame(Date = '2020-01-01',
+                   Depth = 0:5,
+                   TEMP = 29:24)
+
+  sim <- matrix(c(28,28,28,27,25,24),
+               nrow = 6,
+               ncol = 1)
+
+  expect_true(is.object(plot_ts(sim = sim,
+                                  obs = obs,
+                                  sim.start = '2020-01-01',
+                                  sim.end = '2020-01-01',
+                                  plot.start = '2020-01-01',
+                                  plot.end = '2020-01-01',
+                                  ylabel = 'degrees',
+                                  min.depth = 0,
+                                  max.depth = 5,
+                                  by.value = 1)))
+
+})
+
+
+test_that('plot_ts accept right format of date',{
+
+  obs <- data.frame(Date = '2020-01-01',
+                   Depth = 0:5,
+                   TEMP = 29:24)
+
+  sim <- matrix(c(28,28,28,27,25,24),
+               nrow = 6,
+               ncol = 1)
+
+  expect_error(is.object(plot_ts(sim = sim,
+                                   obs = obs,
+                                   sim.start = '01-01-2020',
+                                   sim.end = '2020-01-01',
+                                   plot.start = '2020-01-01',
+                                   plot.end = '2020-01-01',
+                                   ylabel = 'degrees',
+                                   min.depth = 0,
+                                   max.depth = 5,
+                                   by.value = 1)))
+
+})
