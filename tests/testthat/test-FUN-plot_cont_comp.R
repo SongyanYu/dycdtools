68b8628e9698c9257ee89eb11fadcac23e8516d2
Sunny Yu <s2974665@staff.ad.griffith.edu.au>
2022-05-25 16:08:10 +1000

    remove test object

--- tests/testthat/test-FUN-plot_cont_comp.R
+++ tests/testthat/test-FUN-plot_cont_comp.R
@@ -1,36 +1,13 @@
-test_that('plot_cont_comp outputs a plot object',{
-
-  obs <- data.frame(Date = '2020-01-01',
-                   Depth = 0:5,
-                   TEMP = 29:24)
-
-  sim <- matrix(c(28,28,28,27,25,24),
-               nrow = 6,
-               ncol = 1)
-
-  expect_true(is.object(plot_cont_comp(sim = sim,
-                                       obs = obs,
-                                       sim.start = '2020-01-01',
-                                       sim.end = '2020-01-01',
-                                       plot.start = '2020-01-01',
-                                       plot.end = '2020-01-01',
-                                       legend.title = 'degrees',
-                                       min.depth = 0,
-                                       max.depth = 5,
-                                       by.value = 1,
-                                       nlevels = 20)))
-
-})
 
 test_that('plot_cont_comp accept right format of date',{
 
-  obs <- data.frame(Date = '2020-01-01',
-                    Depth = 0:5,
-                    TEMP = 29:24)
+  obs <- data.frame(Date = c(rep('2020-01-01', 6), rep('2020-01-02', 6)),
+                    Depth = rep(0:5, 2),
+                    TEMP = rep(29:24,2))
 
-  sim <- matrix(c(28,28,28,27,25,24),
+  sim <- matrix(c(28,28,28,27,25,24,12,13,14,15,16,17),
                 nrow = 6,
-                ncol = 1)
+                ncol = 2)
 
   expect_error(is.object(plot_cont_comp(sim = sim,
                                        obs = obs,
