0b8632da96302fb0fb1b4f23332b0d8ad6223068
Sunny Yu <s2974665@staff.ad.griffith.edu.au>
2022-05-25 14:13:48 +1000

    unit test profile plot

---
+++ tests/testthat/test-FUN-plot_prof.R
@@ -0,0 +1,23 @@
+
+test_that('plot_prof outputs a plot object',{
+
+  obs = data.frame(Date = '2020-01-01',
+                   Depth = 0:5,
+                   TEMP = 29:24)
+
+  sim = matrix(c(28,28,28,27,25,24),
+               nrow = 6,
+               ncol = 1)
+
+  expect_true(is.object(plot_prof(sim = sim,
+            obs = obs,
+            sim.start = '2020-01-01',
+            sim.end = '2020-01-01',
+            plot.start = '2020-01-01',
+            plot.end = '2020-01-01',
+            xlabel = 'degrees',
+            min.depth = 0,
+            max.depth = 5,
+            by.value = 1)))
+
+})
