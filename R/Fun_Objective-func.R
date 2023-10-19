0011318307b3d610fd447f406f64913fea894b05
SongyanYu <sunny.yu@griffith.edu.au>
2023-10-19 15:07:49 +1000

    replace hydroGOF NSE with a customized one

--- R/Fun_Objective-func.R
+++ R/Fun_Objective-func.R
@@ -74,7 +74,8 @@ objective_fun<-function(sim,
   #---
   # 3. calculate goodness of fit measures
   #---
-  value.NSE <- NSE(var.both$sim, obs = var.both$obs)
+  value.NSW <- 1 - sum((var.both$obs - var.both$sim)^2)/
+    sum((var.both$obs - mean(var.both$obs, na.rm = TRUE))^2)
   value.RMSE <- sqrt(mean((var.both$sim-var.both$obs)^2, na.rm = TRUE))
   value.MAE <- mean(abs(var.both$sim-var.both$obs), na.rm=TRUE)
   value.Pearson <- cor(x = var.both$sim, y = var.both$obs, method = "pearson",
